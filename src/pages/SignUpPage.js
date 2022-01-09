3d6549a03377ec0617149587d33b3b9541268f5a
dnkc <dnkcdesign@gmail.com>
2022-01-09 18:56:28 -0500

    signup component & tests completed

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -8,6 +8,8 @@ const SignUpPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordRepeat, setPasswordRepeat] = useState("");
+  const [apiProgress, setApiProgress] = useState(false);
+  const [signupSuccess, setSignUpSuccess] = useState(false);
 
   useEffect(() => {
     if (password.length > 0 && passwordRepeat.length > 0) {
@@ -21,55 +23,103 @@ const SignUpPage = () => {
     }
   }, [password, passwordRepeat]);
 
-  const submitForm = (e) => {
+  const submitForm = async (e) => {
     e.preventDefault();
     const body = {
       username,
       email,
       password,
     };
-    axios.post("/api/1.0/users", body);
+    setApiProgress(true);
+    try {
+      await axios.post("/api/1.0/users", body).then(() => {
+        setSignUpSuccess(true);
+      });
+    } catch (error) {}
+    // fetch("/api/1.0/users", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(body),
+    // });
     setUsername("");
     setEmail("");
     setPassword("");
     setPasswordRepeat("");
+    setApiProgress(false);
   };
 
   return (
-    <>
-      <form>
-        <h1>Sign Up</h1>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <label htmlFor="email">Email</label>
-        <input
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <label htmlFor="password repeat">Password Repeat</label>
-        <input
-          type="password"
-          value={passwordRepeat}
-          id="password repeat"
-          onChange={(e) => setPasswordRepeat(e.target.value)}
-        />
-        <button disabled={disabled} onClick={(e) => submitForm(e)}>
-          Sign Up
-        </button>
-      </form>
-    </>
+    <div className="col-lg-6 offset-lg-3 col-md-8 offset-md-2">
+      {!signupSuccess && (
+        <form className="card mt-5" data-testid="form-sign-up">
+          <div className="card-header">
+            <h1 className="text-center">Sign Up</h1>
+          </div>
+          <div className="card-body">
+            <div className="mb-3">
+              <label htmlFor="username">Username</label>
+              <input
+                id="username"
+                className="form-control"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email">Email</label>
+              <input
+                id="email"
+                className="form-control"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                className="form-control"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password repeat">Password Repeat</label>
+              <input
+                type="password"
+                value={passwordRepeat}
+                className="form-control"
+                id="password repeat"
+                onChange={(e) => setPasswordRepeat(e.target.value)}
+              />
+            </div>
+            <div className="text-center">
+              <button
+                className="btn btn-primary"
+                disabled={disabled || apiProgress}
+                onClick={(e) => submitForm(e)}
+              >
+                {apiProgress && (
+                  <span
+                    className="spinner-border spinner-border-sm"
+                    role="status"
+                  ></span>
+                )}
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </form>
+      )}
+      {signupSuccess && (
+        <div className="alert alert-success mt-3">
+          Please check your e-mail to activate your account.
+        </div>
+      )}
+    </div>
   );
 };
 
--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -1,7 +1,20 @@
 import SignUpPage from "./SignUpPage";
-import { render, screen } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import axios from "axios";
+import { setupServer } from "msw/node";
+import { rest } from "msw";
+
+// lifecycle describe functions
+// beforeEach
+// beforeAll
+// afterEach
+// afterAll
 
 describe("Sign Up Page", () => {
   describe("Layout", () => {
@@ -53,16 +66,26 @@ describe("Sign Up Page", () => {
     });
   });
   describe("Interactions", () => {
-    it("enables the button when password and password repeat fields have the same value", () => {
-      render(<SignUpPage />);
-      const passwordInput = screen.getByLabelText("Password");
-      const passwordRepeatInput = screen.getByLabelText("Password Repeat");
-      userEvent.type(passwordInput, "P4ssword");
-      userEvent.type(passwordRepeatInput, "P4ssword");
-      const button = screen.queryByRole("button", { name: "Sign Up" });
-      expect(button).toBeEnabled();
+    let requestBody;
+    let counter = 0;
+    const server = setupServer(
+      rest.post("/api/1.0/users", (req, res, ctx) => {
+        requestBody = req.body;
+        counter += 1;
+        return res(ctx.status(200));
+      })
+    );
+    beforeAll(() => {
+      server.listen();
     });
-    it("sends username, email, and password to backend after clicking the button", () => {
+    afterAll(() => {
+      server.close();
+    });
+    beforeEach(() => {
+      counter = 0;
+    });
+    let button;
+    const setup = () => {
       render(<SignUpPage />);
       const usernameInput = screen.getByLabelText("Username");
       const emailInput = screen.getByLabelText("Email");
@@ -72,20 +95,89 @@ describe("Sign Up Page", () => {
       userEvent.type(emailInput, "user1@mail.com");
       userEvent.type(passwordInput, "P4ssword");
       userEvent.type(passwordRepeatInput, "P4ssword");
-      const button = screen.queryByRole("button", { name: "Sign Up" });
+      button = screen.queryByRole("button", { name: "Sign Up" });
+    };
 
-      const mockFn = jest.fn();
-      axios.post = mockFn;
+    it("enables the button when password and password repeat fields have the same value", () => {
+      setup();
+      expect(button).toBeEnabled();
+    });
+    it("sends username, email, and password to backend after clicking the button", async () => {
+      setup();
 
       userEvent.click(button);
 
-      const firstCallofMockFunction = mockFn.mock.calls[0];
-      const body = firstCallofMockFunction[1];
-      expect(body).toEqual({
+      // MOCK SERVER API w/ MSW
+      // does not matter if fetch or axios used
+      await screen.findByText(
+        "Please check your e-mail to activate your account."
+      );
+      expect(requestBody).toEqual({
         username: "user1",
         email: "user1@mail.com",
         password: "P4ssword",
       });
+      //const mockFn = jest.fn();
+
+      // MOCK WINDOWS FETCH
+      //   window.fetch = mockFn;
+      //   const firstCallofMockFunction = mockFn.mock.calls[0];
+      //   const body = JSON.parse(firstCallofMockFunction[1].body);
+      //   expect(body).toEqual({
+      //     username: "user1",
+      //     email: "user1@mail.com",
+      //     password: "P4ssword",
+      //   });
+
+      // // MOCK AXIOS POST
+      //   axios.post = mockFn;
+      //   userEvent.click(button);
+      //   const firstCallofMockFunction = mockFn.mock.calls[0];
+      //   const body = firstCallofMockFunction[1];
+      //   expect(body).toEqual({
+      //     username: "user1",
+      //     email: "user1@mail.com",
+      //     password: "P4ssword",
+      //   });
+    });
+    it("disables button when there is an ongoing api call", async () => {
+      setup();
+
+      userEvent.click(button);
+      userEvent.click(button);
+
+      await new Promise((resolve) => setTimeout(resolve, 500));
+      expect(counter).toBe(1);
+    });
+    // TO DO:
+    xit("displays spinner after clicking the submit", async () => {
+      setup();
+
+      expect(screen.queryByRole("status")).not.toBeInTheDocument();
+      userEvent.click(button);
+      const spinner = await screen.queryByRole("status");
+      expect(spinner).toBeInTheDocument();
+      await screen.findByText(
+        "Please check your e-mail to activate your account."
+      );
+    });
+    it("displays account activation notification after successful sign up request", async () => {
+      setup();
+      const message = "Please check your e-mail to activate your account.";
+      expect(screen.queryByText(message)).not.toBeInTheDocument();
+      userEvent.click(button);
+
+      const text = await screen.findByText(message);
+      expect(text).toBeInTheDocument();
+    });
+    it("hides sign up form after successful sign up request", async () => {
+      setup();
+      const form = screen.getByTestId("form-sign-up");
+      userEvent.click(button);
+      // await waitFor(() => {
+      //   expect(form).not.toBeInTheDocument();
+      // });
+      await waitForElementToBeRemoved(form);
     });
   });
 });
