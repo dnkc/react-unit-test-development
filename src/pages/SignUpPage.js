3254eacfa9b3cf2b78a858293a8e7a14e818aaa3
dnkc <dnkcdesign@gmail.com>
2022-01-09 14:45:08 -0500

    finished layout test, start api tests

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,13 +1,74 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const SignUpPage = () => {
+  const [disabled, setDisabled] = useState(true);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordRepeat, setPasswordRepeat] = useState("");
+
+  useEffect(() => {
+    if (password.length > 0 && passwordRepeat.length > 0) {
+      if (password === passwordRepeat) {
+        setDisabled(false);
+      } else {
+        setDisabled(true);
+      }
+    } else {
+      setDisabled(true);
+    }
+  }, [password, passwordRepeat]);
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    const body = {
+      username,
+      email,
+      password,
+    };
+    axios.post("/api/1.0/users", body);
+    setUsername("");
+    setEmail("");
+    setPassword("");
+    setPasswordRepeat("");
+  };
+
   return (
     <>
-      <h1>Sign Up</h1>
-      <label htmlFor="username">Username</label>
-      <input id="username" />
-      <label htmlFor="email">Email</label>
-      <input id="email" />
+      <form>
+        <h1>Sign Up</h1>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <label htmlFor="password repeat">Password Repeat</label>
+        <input
+          type="password"
+          value={passwordRepeat}
+          id="password repeat"
+          onChange={(e) => setPasswordRepeat(e.target.value)}
+        />
+        <button disabled={disabled} onClick={(e) => submitForm(e)}>
+          Sign Up
+        </button>
+      </form>
     </>
   );
 };
--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -1,5 +1,7 @@
 import SignUpPage from "./SignUpPage";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import axios from "axios";
 
 describe("Sign Up Page", () => {
   describe("Layout", () => {
@@ -18,6 +20,72 @@ describe("Sign Up Page", () => {
       render(<SignUpPage />);
       const input = screen.getByLabelText("Email");
       expect(input).toBeInTheDocument();
+    });
+    it("has password input", () => {
+      render(<SignUpPage />);
+      const input = screen.getByLabelText("Password");
+      expect(input).toBeInTheDocument();
+    });
+    it("has password type for password input", () => {
+      render(<SignUpPage />);
+      const input = screen.getByLabelText("Password");
+      expect(input.type).toBe("password");
+    });
+    it("has password repeat input", () => {
+      render(<SignUpPage />);
+      const input = screen.getByLabelText("Password Repeat");
+      expect(input).toBeInTheDocument();
+    });
+    it("has password type for password repeat input", () => {
+      render(<SignUpPage />);
+      const input = screen.getByLabelText("Password Repeat");
+      expect(input.type).toBe("password");
+    });
+    it("has sign up button", () => {
+      render(<SignUpPage />);
+      const button = screen.queryByRole("button", { name: "Sign Up" });
+      expect(button).toBeInTheDocument(); // jest-dom
+    });
+    it("disables the button initially", () => {
+      render(<SignUpPage />);
+      const button = screen.queryByRole("button", { name: "Sign Up" });
+      expect(button).toBeDisabled(); // jest-dom
+    });
+  });
+  describe("Interactions", () => {
+    it("enables the button when password and password repeat fields have the same value", () => {
+      render(<SignUpPage />);
+      const passwordInput = screen.getByLabelText("Password");
+      const passwordRepeatInput = screen.getByLabelText("Password Repeat");
+      userEvent.type(passwordInput, "P4ssword");
+      userEvent.type(passwordRepeatInput, "P4ssword");
+      const button = screen.queryByRole("button", { name: "Sign Up" });
+      expect(button).toBeEnabled();
+    });
+    it("sends username, email, and password to backend after clicking the button", () => {
+      render(<SignUpPage />);
+      const usernameInput = screen.getByLabelText("Username");
+      const emailInput = screen.getByLabelText("Email");
+      const passwordInput = screen.getByLabelText("Password");
+      const passwordRepeatInput = screen.getByLabelText("Password Repeat");
+      userEvent.type(usernameInput, "user1");
+      userEvent.type(emailInput, "user1@mail.com");
+      userEvent.type(passwordInput, "P4ssword");
+      userEvent.type(passwordRepeatInput, "P4ssword");
+      const button = screen.queryByRole("button", { name: "Sign Up" });
+
+      const mockFn = jest.fn();
+      axios.post = mockFn;
+
+      userEvent.click(button);
+
+      const firstCallofMockFunction = mockFn.mock.calls[0];
+      const body = firstCallofMockFunction[1];
+      expect(body).toEqual({
+        username: "user1",
+        email: "user1@mail.com",
+        password: "P4ssword",
+      });
     });
   });
 });
